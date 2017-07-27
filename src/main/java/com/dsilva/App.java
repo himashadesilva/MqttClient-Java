75ee4a4bd2ffd17763fcb536b2e5f794590005ec
himasha.desilva <himashasds@gmail.com>
2017-07-28 01:36:03 +0530

    Initial commit

---
+++ src/main/java/com/dsilva/App.java
@@ -0,0 +1,13 @@
+package com.dsilva;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
