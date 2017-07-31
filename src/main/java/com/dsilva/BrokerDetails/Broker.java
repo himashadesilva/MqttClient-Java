9ce1aaff21a451457cc8bf4edb10dbdabb7ccc76
himasha.desilva <himashasds@gmail.com>
2017-07-31 19:43:14 +0530

    Reformatted

--- src/main/java/com/dsilva/BrokerDetails/Broker.java
+++ src/main/java/com/dsilva/BrokerDetails/Broker.java
@@ -4,6 +4,7 @@ package com.dsilva.BrokerDetails;
  * Created by Himasha on 7/28/2017.
  */
 public class Broker {
+
     public static final String BROKER = "tcp://0.0.0.0:1883";
     public static final String USER_NAME = "admin";
     public static final String PASSWORD = "admin";
