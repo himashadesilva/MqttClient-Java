433982604b8ddce7fbf22c1f8c2fae9d8ded5ac7
himasha.desilva <himashasds@gmail.com>
2017-07-31 19:39:28 +0530

    Change main method of calling sub and pub.

--- src/main/java/com/dsilva/Main/Main.java
+++ src/main/java/com/dsilva/Main/Main.java
@@ -1,13 +1,65 @@
 package com.dsilva.Main;
 
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+import java.util.Scanner;
+
 /**
- * Hello world!
+ * Mqtt Client - Java
  *
+ * @author Himasha De Silva
  */
-public class Main
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Main {
+    private static final String PUBLISH = "publish";
+    private static final String SUBSCRIBE = "subscribe";
+    private static final String DASH = "-";
+    private static final String HELP = "help";
+    private static final String MESSAGE = "m";
+    private static final String FILE = "f";
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Publish or Subscribe?");
+
+        String type = sc.nextLine();
+        System.out.println("topic ?");
+
+        String topic = sc.nextLine();
+        String message;
+        System.out.println("client Id?");
+        String clientId = sc.nextLine();
+
+        //if (args[0].split(DASH)[1].equalsIgnoreCase(PUBLISH) && args.length==5)
+        if (type.equalsIgnoreCase(PUBLISH))
+        {
+            try {
+                while (true) {
+                    System.out.println("messsage :");
+                    message = sc.nextLine();
+                    Publish publish = new Publish(true, clientId);
+                    if(message.equalsIgnoreCase("exit")){
+                        publish.close();
+                        break;
+                    }
+                    publish.sendMessage(topic, message);
+                }
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
+        } else if (type.equalsIgnoreCase(SUBSCRIBE)) {
+            try {
+                Subscribe subscribe = new Subscribe(true, clientId, topic);
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
+        }
+        /*else if(args[0].split(DASH)[1].equalsIgnoreCase(HELP)){
+            System.out.println("usage : ");
+        }
+        else {
+            System.out.println("invalid arguments");
+        }*/
+
     }
 }
